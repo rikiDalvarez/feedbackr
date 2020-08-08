68bbab27ff1f93110e07dd4e1c3a11cd10c349c4
marijana <marijana.delbianco@gmail.com>
2020-08-08 17:41:54 +0200

    test: added test for handleSubmit in formFunctions

--- feedbackr/src/CreateQuestionForm/formFunctions.test.js
+++ feedbackr/src/CreateQuestionForm/formFunctions.test.js
@@ -1,27 +1,47 @@
 import { handleChange, handleSubmit} from './formFunctions';
 
-
 describe('handleChange', () => {
-    let question = {question: ''};
-    const setQuestion = (value) => {question = value}
+  let question = {question: ''};
+  const setQuestion = (value) => {question = value}
 
-    it('target.name !== answerOptions', () => {
-        const event = {target: { value: 'banana', name: 'question'}}
+  it('handleChange for question input', () => {
+    const event = {target: { value: 'banana', name: 'question'}}
+    handleChange(event, null, setQuestion, question);
+    expect(question.question).toBe('banana');
+  })
 
-        handleChange(event, null, setQuestion, question);
-        console.log(question);
+  it('handleChange for answer input', () => {
+    const event = {target: { value: 'banana', name: 'answerOptions'}}
+    handleChange(event, null, setQuestion, question);
+    expect(question.answerOptions).not.toBe('banana');
+  })
 
-        expect(question.question).toBe('banana');
+})
 
-    })
+describe('handleSubmit', () => {
+  let question = {question: ''};
+  const setQuestion = (value) => {question = value}
+  const initialQuestion = {
+    question: "",
+    answerOptions: [{
+      value: 0,
+      label: ""
+    }],
+    correctAnswer: null,
+    tags: "",
+    time: 0,
+  }
+  const event = {target: { value: 'banana'}, preventDefault: ()=> jest.fn()}
+  const handleQuestionSubmit = (question) => {
+    let newQuestion = {
+      quizId: 1,
+      question: question
+    }
+  }
 
-    it('target.name !== answerOptions', () => {
-        const event = {target: { value: 'banana', name: 'answerOptions'}}
+  it('submit question to database and set back to initial state', () => {
+    handleSubmit(event, handleQuestionSubmit, setQuestion, question, initialQuestion)
+    expect(question.question).toBe('')
+  })
 
-        handleChange(event, null, setQuestion, question);
-        console.log(question);
-
-        expect(question.answerOptions).not.toBe('banana');
-
-    })
 })
