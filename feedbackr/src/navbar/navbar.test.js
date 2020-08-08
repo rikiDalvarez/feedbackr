05105394e8352a91c73687f29728f122988975e5
marijana <marijana.delbianco@gmail.com>
2020-08-08 15:47:23 +0200

    fix: fixed minor issues in quizCard.test and navbar.test

--- feedbackr/src/navbar/navbar.test.js
+++ feedbackr/src/navbar/navbar.test.js
@@ -4,22 +4,20 @@ import { render, screen } from '@testing-library/react';
 import NavBar from "./navbar";
 
 describe('Navbar', () => {
-  it('shows logo, create and view quizes', () => {
-    render(<NavBar/>);
-    screen.debug();
-  })
 
   it('check if the logo is rendered', () => {
+    render(<NavBar/>);
     expect(screen.getByText('Feedbackr')).toBeInTheDocument();
   })
 
   it('check if creat quizes are in the document', () => {
-    expect(screen.getByText('Create quiz')).toBeInTheDocument();
+    render(<NavBar/>);
+    expect(screen.getByText('Create Quiz')).toBeInTheDocument();
   })
 
   it('check if view quizes are in the document', () => {
+    render(<NavBar/>);
     expect(screen.getByText('View quizzes')).toBeInTheDocument();
   })
-
 
 })
