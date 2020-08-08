7aadaa71b8bf8629389cb47e84d7c84599017b08
Martin Rodl <axsis@centrum.cz>
2020-08-08 11:51:40 +0200

    Test-add integration test for App.js

--- feedbackr/src/App.test.js
+++ feedbackr/src/App.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { getAllQuizzes } from './apiService';
+import ReactDOM from 'react-dom';
+import QuizList from './QuizList/quizList';
+import DoQuiz from './DoQuiz/doQuiz';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./apiService');
+
+const fakeQuizzes = [
+  { name: 'name1', tags: 'first tag', questions: [] },
+  { name: 'name2', tags: 'second tag', questions: [] },
+];
+
+getAllQuizzes.mockResolvedValue(fakeQuizzes);
+
+describe('App.js ', () => {
+  it('loading articles', async () => {
+    render(<App />);
+    expect(getAllQuizzes).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders navbar and proper href', () => {
+    render(<App />);
+
+    expect(screen.getByText('Create Quiz').getAttribute('href')).toBe(
+      '/create-quiz'
+    );
+    expect(screen.getByText('View quizzes').getAttribute('href')).toBe(
+      '/view-quizzes'
+    );
+  });
+
+  test('rendering child component', async () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<QuizList quizList={fakeQuizzes} />, div);
+    ReactDOM.render(<DoQuiz quiz={fakeQuizzes[0]} />, div);
+  });
 });
